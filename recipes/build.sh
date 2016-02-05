be8dc6a49d5236dd6aad3cdc5e6e99924a854ca9
Jaime Huerta-Cepas <jhcepas@gmail.com>
2016-02-05 17:56:14 +0100

    udpdate recipes

--- recipes/build.sh
+++ recipes/build.sh
@@ -1,6 +1,7 @@
-for x in *.ipynb; do ipython nbconvert --to=html $x; done
-scp *.html ete:web/static/cookbook/
-rsync -r data/ ete:web/static/cookbook/data/
+for x in index.ipynb ete*.ipynb; do ipython nbconvert --to=html $x; done
+
+#scp *.html ete:web/static/cookbook/
+#rsync -r data/ ete:web/static/cookbook/data/
 
 
 
