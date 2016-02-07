8b84fbadacf780c069fec9e0b3f01699defcc22a
Jaime Huerta-Cepas <jhcepas@gmail.com>
2016-02-07 09:42:25 +0100

    clean html files0

--- recipes/build.sh
+++ recipes/build.sh
@@ -1,4 +1,6 @@
 for x in index.ipynb ete*.ipynb; do ipython nbconvert --to=html $x; done
+sed -i.delete 's/<html>//g' *.html
+sed -i.delete 's/<\/html>//g' *.html
 
 #scp *.html ete:web/static/cookbook/
 #rsync -r data/ ete:web/static/cookbook/data/
