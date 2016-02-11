8146f0f0e729cb864303d51dd151257410dcd1ea
Jaime Huerta-Cepas <jhcepas@gmail.com>
2016-02-11 23:24:19 +0100

    more html cleaning

--- recipes/build.sh
+++ recipes/build.sh
@@ -2,6 +2,9 @@ for x in index.ipynb ete*.ipynb; do ipython nbconvert --template full --to=html 
 sed -i.delete 's/<html>//g' *.html
 sed -i.delete 's/<\/html>//g' *.html
 sed -i.delete 's/<\!DOCTYPE html>//g' *.html
+sed -i.delete 's/padding: 8px;//g' *.html
+sed -i.delete 's/require.min.js//g' *.html
+
 rm *.delete
 #scp *.html ete:web/static/cookbook/
 #rsync -r data/ ete:web/static/cookbook/data/
